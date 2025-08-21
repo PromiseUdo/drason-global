c8653b95c8d374108ae4fa5fb2717d3e8d358096
PromiseUdo <info.promiseudo@gmail.com>
2025-08-21 13:23:07 +0100

    update

--- app/about-us/components/page-header.tsx
+++ app/about-us/components/page-header.tsx
@@ -23,17 +23,26 @@ const PageHeader: React.FC<PageHeaderProps> = ({
 }) => {
   return (
     <section
-      className="py-20 bg-cover h-[400px] flex items-center justify-center bg-center bg-no-repeat relative"
+      className="py-20 bg-cover h-[100vh] flex items-center justify-center bg-center bg-no-repeat relative"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       {/* Overlay */}
-      <div className="absolute inset-0 bg-black/50" />
+      <div className="absolute inset-0 bg-black/80" />
       <MaxWidthWrapper className="">
-        <div className="relative z-10 flex  flex-col items-center justify-center">
+        <div className="relative z-10 flex gap-10 flex-col items-center justify-center">
           {/* <Breadcrumb items={breadcrumbItems} /> */}
           <h2 className="text-3xl md:text-4xl font-bold text-white font-heading">
             {title}
           </h2>
+
+          <p className="text-center text-white text-[15px] leading-[22.5px] max-w-lg">
+            {" "}
+            Drason Global Limited is an indigenous company incorporated in
+            Nigeria in 2020 that provides benchmark quality performance to our
+            clients based on innovative solutions, safety, environmental
+            responsibility, dedicated professional employees that are guided by
+            Godly principles and modern efficient equipment.
+          </p>
         </div>
       </MaxWidthWrapper>
     </section>
