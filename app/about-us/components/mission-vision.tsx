1a1372865699d746f8395e7e14c6473d189dc3d0
PromiseUdo <info.promiseudo@gmail.com>
2025-08-21 13:20:00 +0100

    update

--- app/about-us/components/mission-vision.tsx
+++ app/about-us/components/mission-vision.tsx
@@ -10,21 +10,24 @@ const MissionVision = () => {
 
           <div className="flex-1 bg-[#890c25] p-6 ">
             <h2 className="text-2xl font-bold mb-4 font-heading">Vision</h2>
-            <p className="">
-              We envision a world where technology and human ingenuity combine
-              to solve the greatest challenges and unlock limitless
-              possibilities.
+            <p className="text-[15px]">
+              To be the foremost integrated services company in the oil and gas
+              and energy sectors, setting unparalleled standards and driving
+              global impact through innovation, efficiency, and sustainable
+              growth.
             </p>
           </div>
 
           {/* Vision Div */}
 
-          <div className="flex-1 bg-[#00984A] p-6">
+          <div className="flex-1 bg-[#000C1F] p-6">
             <h2 className="text-2xl font-bold mb-4 font-heading">Mission</h2>
-            <p className="">
-              Our mission is to empower individuals and organizations with
-              innovative solutions that drive progress and create lasting
-              impact.
+            <p className="text-[15px]">
+              To optimize operations, deliver exceptional value to clients,
+              drive sustainable growth, and make a positive global impact.
+              Through strategic acquisitions and a commitment to excellence, we
+              aim to serve our clients in Nigeria, expand regionally in West
+              Africa, and establish a strong presence in international markets.
             </p>
           </div>
         </div>
